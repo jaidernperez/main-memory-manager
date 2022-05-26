ae43d947f6e857d5d51d350b3fde3427772c86ec
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-05-26 17:20:48 -0500

    Initial commit

---
+++ src/main/java/com/usta/mainmemorymanager/MainMemoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.usta.mainmemorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainMemoryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainMemoryManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usta/mainmemorymanager/MainMemoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usta.mainmemorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainMemoryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
